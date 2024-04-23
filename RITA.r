c4b4afa200a61de2975772c66d96486dfd7f7146
Rodrigo Vidal <55030648+rvidalh94@users.noreply.github.com>
2024-04-22 22:52:34 -0400

    Add co-authored and co-authored institution avg and other fixes

--- RITA.r
+++ RITA.r
@@ -3,7 +3,7 @@ library("plotly")  # Grapgh Library
 library("dplyr")   # Data sorting
 
 # Read database from the dir where the file is
-datos <- read.csv("BD_RITA05 - BD_RITA05.csv", sep = ",", header = TRUE, encoding = "UTF-8")
+datos <- read.csv("BD_RITA04.csv", sep = ",", header = TRUE, encoding = "UTF-8")
 # Filter the dataset for the desired year -> 2022
 desired_year <- 2022
 
@@ -38,14 +38,8 @@ datos_tmp <- datos
 datos_year <- datos_tmp %>%
   filter(year == desired_year)
 
-# Splitting authors column and unnesting to create one row per author
-authors_split <- datos_year %>%
-  mutate(authors = strsplit(as.character(authors), ", ")) %>%
-  tidyr::unnest(authors) %>%
-  mutate(authors = trimws(authors))  # Remove extra whitespaces
-
 # Counting the number of publications per author
-publications_per_author <- authors_split %>%
+publications_per_author <- datos_year %>%
   group_by(authors) %>%
   summarize(publications = n())
 
@@ -59,13 +53,13 @@ data_year <- datos %>%
 publications_authors_for_year <- data_year %>%
   group_by(year) %>%
   summarize(
-    total_publicaciones = n(),              # Counting the total number of publications
-    total_autores = n_distinct(authors)  # Counting the number of unique authors
+    total_publicaciones = n(), # Counting the total number of publications
+    total_autores = n_distinct(authors) # Counting the number of unique authors
   )
 
 publications_authors_for_year
 
-# Calculating the ratio of total publications to total authors
+# Calculating the avg of total publications to total authors
 ratio_publications_to_authors <- publications_authors_for_year$total_publicaciones / publications_authors_for_year$total_autores
 
 result = paste("Promedio publicaciones por autor:", round(ratio_publications_to_authors, 2))
@@ -76,15 +70,10 @@ datos_tmp <- datos
 datos_year <- datos_tmp %>%
   filter(year == desired_year)
 
-# Splitting authors column and unnesting to create one row per author
-institution_split <- datos_year %>%
-  mutate(institution = strsplit(as.character(institution), ", ")) %>%
-  tidyr::unnest(institution)
-
 # Counting the number of publications per author
-publications_per_institution <- institution_split %>%
-  distinct(doc, .keep_all = TRUE) %>%
+publications_per_institution <- datos_year %>%
   group_by(institution) %>%
+  distinct(doc, .keep_all = TRUE) %>%
   summarize(publications = n())
 
 publications_per_institution
@@ -92,4 +81,68 @@ publications_per_institution
 # Calculating the average number of publications per author
 average_publications_per_institution <- mean(publications_per_institution$publications)
 
+datos_tmp <- datos
 
+datos_year <- datos_tmp %>%
+  filter(year == desired_year)
+
+# Counting the number of publications per author
+publications_per_country <- datos_year %>%
+  group_by(country) %>%
+  distinct(doc, .keep_all = TRUE) %>%
+  summarize(publications = n())
+
+publications_per_country
+
+# Calculating the average number of publications per author
+average_publications_per_country <- mean(publications_per_country$publications)
+
+datos_tmp <- datos
+
+datos_year <- datos_tmp %>%
+  filter(year == desired_year)
+
+# Group the data by publication ID and count the number of rows for each publication
+publication_counts <- datos_year %>%
+  group_by(doc) %>%
+  summarize(num_authors = n())
+
+# Count the number of publications with more than one author
+total_coauthored_publications <- sum(publication_counts$num_authors > 1)
+
+# Total publicataions
+total_pubs <- datos_year %>%
+  summarize(total_publications = n_distinct(doc))
+print(paste("Total de publicaciones:", total_pubs))
+
+# Total number of co-authored publication
+print(paste("Total de publicaciones con coautoría:", total_coauthored_publications))
+
+# Average of co-authored publications
+avg_coauthored = (total_coauthored_publications/total_pubs) * 100
+print(paste("Promedio de publicaciones con coautoría:", round(avg_coauthored, 2)))
+
+datos_tmp <- datos
+
+datos_year <- datos_tmp %>%
+  filter(year == desired_year)
+
+# Group the data by publication ID and count the number of rows for each publication
+publication_counts <- datos_year %>%
+  group_by(doc) %>%
+  summarize(num_institution = n_distinct(institution), .groups = 'drop')
+
+# Count the number of publications with more than one author
+total_co_institution_publications <- sum(publication_counts$num_institution > 1)
+
+# Total publicataions
+total_pubs <- datos_year %>%
+  summarize(total_publications = n_distinct(doc))
+print(paste("Total de publicaciones:", total_pubs))
+
+# Total number of co-institution publication
+print(paste("Total de publicaciones con coautoría institucional:", total_co_institution_publications))
+
+# Average of co-institution publications
+avg_coauthored = (total_co_institution_publications/total_pubs) * 100
+print(paste("Promedio de publicaciones con coautoría institucional:", round(avg_coauthored, 2)))
