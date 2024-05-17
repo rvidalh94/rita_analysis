643a574ca7964a2abee01ed82409e81838f824b6
rvidalh94 <rodrigo.vidal.h@usach.cl>
2024-05-16 21:26:54 -0400

    r dependencies

---
+++ install.R
@@ -0,0 +1,6 @@
+install.packages(c(
+  "tidyverse",    # Collection of R packages for data manipulation and visualization
+  "ggplot2",      # Package for creating graphics
+  "dplyr",        # Package for data manipulation
+  "plotly",        # Package for reading data
+))
